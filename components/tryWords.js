aaa2b63ac5155e306e1356019de7e2a948d38580
Ivan <hellopaidco@gmail.com>
2022-09-21 17:40:34 +0100

    github link

--- components/tryWords.js
+++ components/tryWords.js
@@ -20,20 +20,22 @@ export default function Example({ words, rank }) {
       return Object.values(b)[0] - Object.values(a)[0];
     });
 
-    console.log(score);
-
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 10; i++) {
       if (score[i] !== undefined) {
         tryWords.push(Object.keys(score[i])[0]);
       }
     }
+    tryWords = tryWords.sort(() => Math.random() - 0.5);
   }
 
   return (
     <div className='mx-4 my-12'>
-      <h2 className='text-2xl text-center font-bold mb-4'>Words to Try</h2>
+      <h2 className='text-2xl text-center font-bold mb-1'>Words to try</h2>
+      <div className='text-sm text-center text-gray-500 mb-4 '>
+        Try familiar words first
+      </div>
       <div className='w-full grid grid-cols-1 sm:grid-cols-6 gap-2'>
-        {tryWords.map((word) => (
+        {tryWords.slice(0, 6).map((word) => (
           <div
             key={word}
             className='relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-green-500 focus-within:ring-offset-2 hover:border-gray-400'>
